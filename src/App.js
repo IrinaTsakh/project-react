f2464e5229208df940a66155fa7e6b859aa13a8d
IrinaTsakh <tsachirina@gmail.com>
2022-10-20 16:44:54 +0300

    added a link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://zingy-cucurucho-d62fff.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Iryna Tsakh
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/IrinaTsakh/project-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
